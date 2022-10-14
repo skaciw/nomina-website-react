22cac5b9ea090b1d6f1cf856c5297ebf889cfaec
Jennifer Goldwin <jennifer.goldwin@binus.ac.id>
2022-10-14 10:00:30 +0700

    update loading assets

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,16 +1,20 @@
-import loading from  '../assets/Spin-1s-200px.gif'
+import loading from "../assets/Spinner-1s-800px.gif";
 
-const Loading=()=>{
-    // const styles = StyleSheet.create({ container: { display: 'flex', flex: 1, justifyContent: 'center', alignItems: 'center', }, logo: { width: 220, height: 300, marginTop: -50, }, });
-    return(
-        <view style={{ height:"100vh", display:"flex",justifyContent:"center",alignItems:"center",flex:"1"}}>
-            
-            <img src={loading} alt=""/>
-        </view>
-    );
+const Loading = () => {
+  // const styles = StyleSheet.create({ container: { display: 'flex', flex: 1, justifyContent: 'center', alignItems: 'center', }, logo: { width: 220, height: 300, marginTop: -50, }, });
+  return (
+    <view
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flex: "1",
+      }}
+    >
+      <img width={200} height="auto" src={loading} alt="" />
+    </view>
+  );
+};
 
-}
-
-
-
-export default Loading
+export default Loading;
